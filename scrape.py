6d9bde09be8039bf849a2d96ab8393a6ce237449
Earthling <Earthling@EARTHLING-PC>
2018-08-22 00:04:13 -0700

    Added functionality to write a csv file with scrapped data.

--- scrape.py
+++ scrape.py
@@ -1,11 +1,42 @@
 from bs4 import BeautifulSoup
-import requests
+import requests, csv
 
 source = requests.get('http://eclecticexistential.github.io/').text
 soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('p_scrape.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Headline', 'Summary'])
+
 for match in soup.find_all('div', class_='col'):
-    print(match.p.text)
-    print()
+    headline = match.h3.text
+    summary = match.p.text
+    csv_writer.writerow([headline, summary])
+
+csv_file.close()
+
+with open('p_scrape.csv') as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        print(row)
+
+# to get embedded video:
+# -grab url from iframe
+# vid_src = article.find("iframe", class_="youtube-player")['src']
+# vid_id = vid_src.split('/')[4]
+# vid_id = vid_id.split('?')[0]
+# yt_link = f'https://youtube.com/watch?v={vid_id}'
+
+# to skip by missing info put into try/except
+
+# try:
+#    source = requests.get('http://eclecticexistential.github.io/').text
+#    soup = BeautifulSoup(source, 'lxml')
+#    for match in soup.find_all('div', class_='col'):
+#        print(match.p.text)
+#        print()
+# except Exception as e:
+#    yt_link = None (rather than pass)
 
 # source = requests.get('http://eclecticexistential.github.io/').text <- get website data
 # soup = BeautifulSoup(source, 'lxml') <- get data with tags
