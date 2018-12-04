8a2bcbcfde4ebd6ae407dfd00dc87d2e08d83d54
Earthling <Earthling@EARTHLING-PC>
2018-12-04 14:20:41 -0800

    December updates.

--- hacknews/hacknews.py
+++ hacknews/hacknews.py
@@ -3,28 +3,18 @@ import requests
 import csv
 import re
 
-python = []
-selenium = []
-react = []
-javascript = []
-
-def get_python(name, array):
-	source = requests.get('https://news.ycombinator.com/item?id=18354503').text
+def get_python():
+	source = requests.get('https://news.ycombinator.com/item?id=18589702').text
 	soup = BeautifulSoup(source, 'lxml')
+	csv_file = open('remoteDec.csv', 'w')
+	csv_writer = csv.writer(csv_file)
 	for group in soup.find_all('span', class_='commtext c00'):
 		if group.findAll(text=re.compile('REMOTE')) or group.findAll(text=re.compile('remote')):
-			if group.findAll(text=re.compile(name)):
-				array.append(group.text)
-	csv_file = open('{0}Nov.csv'.format(name), 'w')
-	csv_writer = csv.writer(csv_file)
-	if array != []:
-		for pos in array:
 			try:
-				csv_writer.writerow([pos])
-			except:
-				continue
+				csv_writer.writerow([group.text])
+			except UnicodeEncodeError:
+				apple = group.text
+				apple = apple.replace('\u2028', '')
+				csv_writer.writerow([apple])
 		
-get_python('ython', python)
-get_python('elenium', selenium)
-get_python('eact', react)
-get_python('avascript', javascript)
+get_python()
