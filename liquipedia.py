1f6520a2571342c5d97435fee39c75d3315f4dc9
Earthling <Earthling@EARTHLING-PC>
2018-12-24 01:35:33 -0800

    developed function to scrape stats to see how many times a player has faced an opponent.

--- liquipedia.py
+++ liquipedia.py
@@ -3,9 +3,17 @@ import requests
 import csv
 import re
 
-source = requests.get('https://liquipedia.net/fighters/Tokido/Results').text
-soup = BeautifulSoup(source, 'lxml')
-table = soup.find('table')
-for section in table.find_all('tr'):
-	for data in section.find_all('td'):
-		print(data.text)
+def player_lookup(name, opp):
+	source = requests.get('https://liquipedia.net/fighters/{0}/Results'.format(name)).text
+	soup = BeautifulSoup(source, 'lxml')
+	table = soup.find('table')
+	for section in table.find_all('tr'):
+		for data in section.find_all('td'):
+			links = data.find_all('a')
+			for link in links:
+				if link.text == opp:
+					data = section.text
+					data = data.replace('\n', ' ').replace('B3', '').replace('A3', '').replace('A1','').replace('A2','').replace('A9','')
+					print(data)
+					
+player_lookup("Tokido","Infiltration")
